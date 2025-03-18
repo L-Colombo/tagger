b4377503324193a0672683f463683fb581856b77
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-03-18 16:25:49 +0100

    test(io): write basic test suite for module `tagger::io`

---
+++ src/io.rs
@@ -0,0 +1,9 @@
+use crate::config::Userconfig;
+
+pub fn get_all_tags(_cfg: Userconfig) -> Option<Vec<String>> {
+    todo!()
+}
+
+pub fn get_tags_from_file(_file: String) -> Option<Vec<String>> {
+    todo!()
+}
---
+++ tests/test_io.rs
@@ -0,0 +1,88 @@
+use relative_path::RelativePath;
+use std::env::current_dir;
+use tagger::{config::*, io::*};
+
+#[test]
+fn get_tags_from_all_files() {
+    let project_root_directory = current_dir().unwrap();
+    let org_dir_path = RelativePath::new("/tests/org_files/")
+        .to_path(&project_root_directory)
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    let cfg: Userconfig = Userconfig {
+        org_directory: org_dir_path,
+        exclude_files: None,
+        exclude_pattern: None,
+    };
+
+    assert_eq!(
+        get_all_tags(cfg),
+        Some(vec![
+            String::from("bar_file_1"),
+            String::from("bar_file_2"),
+            String::from("baz_file_1"),
+            String::from("baz_file_2"),
+            String::from("foo_file_1"),
+            String::from("foo_file_2"),
+            String::from("non_delimiters_file_1"),
+            String::from("non_delimiters_file_2"),
+            String::from("trailing_baz_file_1"),
+            String::from("trailing_baz_file_2"),
+            String::from("trailing_foo_file_1"),
+            String::from("trailing_foo_file_2"),
+        ])
+    )
+}
+
+#[test]
+fn get_tags_from_all_files_with_exclude_files() {
+    let project_root_directory = current_dir().unwrap();
+    let org_dir_path = RelativePath::new("/tests/org_files/")
+        .to_path(&project_root_directory)
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    let cfg: Userconfig = Userconfig {
+        org_directory: org_dir_path,
+        exclude_files: Some(vec![String::from("org_file2.org")]),
+        exclude_pattern: None,
+    };
+
+    assert_eq!(
+        get_all_tags(cfg),
+        Some(vec![
+            String::from("bar_file_1"),
+            String::from("baz_file_1"),
+            String::from("foo_file_1"),
+            String::from("non_delimiters_file_1"),
+            String::from("trailing_baz_file_1"),
+            String::from("trailing_foo_file_1"),
+        ])
+    )
+}
+
+#[test]
+fn get_tags_from_one_file_that_has_tags() {
+    let file: String = String::from("org_file1.org");
+
+    assert_eq!(
+        get_tags_from_file(file),
+        Some(vec![
+            String::from("bar_file_1"),
+            String::from("baz_file_1"),
+            String::from("foo_file_1"),
+            String::from("non_delimiters_file_1"),
+            String::from("trailing_baz_file_1"),
+            String::from("trailing_foo_file_1"),
+        ])
+    )
+}
+
+#[test]
+fn get_tags_from_one_file_that_has_no_tags() {
+    let file: String = String::from("org_file_1.org");
+    assert_eq!(get_tags_from_file(file), None)
+}
